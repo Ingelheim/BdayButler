88675515cc45aa4bc83038d3dc1fa2b29e48ccef
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2013-12-10 13:49:28 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdayButler::Application.config.secret_token = 'fb2ab6bdec8344a2a39ea9edf06edd0df1c7e5e83334c402184051ad16a4d14aae751d77f6077d3afa7765c908efedc9efe1b221a8e7b6b18c7758fd8a1dd10d'
